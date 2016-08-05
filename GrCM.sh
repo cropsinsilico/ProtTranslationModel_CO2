04f2f8a8c5e0f05dadb91e1352936e054725ce8c
kkavya03 <kkannan2@illinois.edu>
2016-08-05 17:07:22 -0500

    removed old model outputs and inputs and changes path name in model bash script

--- GrCM.sh
+++ GrCM.sh
@@ -2,4 +2,4 @@
 
 # ----------------Your Commands------------------- #
 . ../../RMQ-support/interface/setup.sh
-../../RMQ-support/interface/PsiRun.py GrCM.yml > ./Output/GrCM_log.txt
+PsiRun.py GrCM.yml > ./Output/GrCM_log.txt
