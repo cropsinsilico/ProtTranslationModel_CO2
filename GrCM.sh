d585f03dda6bd74b65125426c7c0aae7c0cf00bd
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2016-08-08 09:09:12 -0500

    Adjust paths in GrCM driver

--- GrCM.sh
+++ GrCM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # ----------------Your Commands------------------- #
-. ../../raila/Plants_in_Silico/RMQ-support/interface/setup.sh
-../../raila/Plants_in_Silico/RMQ-support/interface/PsiRun.py GrCM.yml > ./Output/GrCM_log.txt
+. ../../RMQ-support/interface/setup.sh
+../../RMQ-support/interface/PsiRun.py GrCM.yml > ./Output/GrCM_log.txt
