640df8a48e789b73bb7d2ad6bdb6c9c042ac98db
Meagan Lang <langmm.astro@gmail.com>
2018-04-16 18:17:42 +0000

    Added more elegant version of integrated model to GrCM_plain

--- src/GrCM_plain.py
+++ src/GrCM_plain.py
@@ -6,6 +6,11 @@ from scipy import integrate
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from cis_interface.interface.CisInterface import *
+
+
+_save_trajectories = False
+_plot_trajectories = False
 
 
 def Protein_translation_RNA(t, y, L, U, D, mRNA):
@@ -30,12 +35,22 @@ def Protein_translation_RNA(t, y, L, U, D, mRNA):
 TDIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def main():
-    # read input data as pandas df
-    data_static = pandas.read_csv(
-        os.path.join(TDIR, "..", "Input", "GrCM_static.txt"), sep="\t")
-    data_inp = pandas.read_csv(
-        os.path.join(TDIR, "..", "Input", "GrCM_input.txt"), sep="\t")
+def GrCM(data_static, data_inp,
+         save_trajectories=True, plot_trajectories=True):
+    r"""Integrate input, getting trajectories for the integration.
+
+    Args:
+        data_static (pandas.DataFrame): Static information.
+        data_inp (pandas.DataFrame): Initial values.
+        save_trajectories (bool, optional): If True, the trajectories will
+            be output to disk. Defaults to True.
+        plot_trajectories (bool, optional): If True, the trajectories will
+            be plotted. Defaults to True.
+
+    Outputs:
+        pandas.DataFrame: The final values after integration.
+
+    """
 
     r1 = integrate.ode(Protein_translation_RNA).set_integrator(
         'vode', method='bdf')
@@ -87,36 +102,67 @@ def main():
     data_static.loc[:, 'new_prot_levels_Amb'] = list1[-1, :]
     data_static.loc[:, 'new_prot_levels_Ele'] = list2[-1, :]
     data_static.loc[:, 'Ele:Amb'] = list2[-1, :] / list1[-1, :]
-    data_static.to_csv(
-        os.path.join(TDIR, "..", "Output", "GrCM_output.txt"),
-        index=False, sep="\t")
+    if save_trajectories:
+        data_static.to_csv(
+            os.path.join(TDIR, "..", "Output", "GrCM_output.txt"),
+            index=False, sep="\t")
 
     # All done!  Plot the trajectories in separate plots:
-    fig = plt.figure(figsize=(16, 12))
-    gs = gridspec.GridSpec(2, 4)
-    ax1 = fig.add_subplot(gs[0])
-    ax1.semilogx(t, list1)
-    ax1.set_xlim(t_start, t_final)
-    ax1.set_xlabel('Time [minutes]')
-    ax1.set_ylabel('Concentration')
-    ax1.grid('on')
+    if plot_trajectories:
+        fig = plt.figure(figsize=(16, 12))
+        gs = gridspec.GridSpec(2, 4)
+        ax1 = fig.add_subplot(gs[0])
+        ax1.semilogx(t, list1)
+        ax1.set_xlim(t_start, t_final)
+        ax1.set_xlabel('Time [minutes]')
+        ax1.set_ylabel('Concentration')
+        ax1.grid('on')
 
-    ax2 = fig.add_subplot(gs[1])
-    ax2.semilogx(t, list2, 'r')
-    ax2.set_xlim(t_start, t_final)
-    ax2.set_xlabel('Time [minutes]')
-    ax2.set_ylabel('Concentration')
-    ax2.grid('on')
+        ax2 = fig.add_subplot(gs[1])
+        ax2.semilogx(t, list2, 'r')
+        ax2.set_xlim(t_start, t_final)
+        ax2.set_xlabel('Time [minutes]')
+        ax2.set_ylabel('Concentration')
+        ax2.grid('on')
 
-    for i in range(2, 8):
-        ax = fig.add_subplot(gs[i])
-        ax.semilogx(t, list2[:, i + 1], 'r')
-        ax.set_xlim(t_start, t_final)
-        ax.set_xlabel('Time [minutes]')
-        ax.grid('on')
+        for i in range(2, 8):
+            ax = fig.add_subplot(gs[i])
+            ax.semilogx(t, list2[:, i + 1], 'r')
+            ax.set_xlim(t_start, t_final)
+            ax.set_xlabel('Time [minutes]')
+            ax.grid('on')
 
-    plt.savefig('Protein_ODE_result.png', bbox='tight')
+        plt.savefig('Protein_ODE_result.png', bbox='tight')
+    return data_static
 
+    
+def main():
+    # read input data as pandas df & run with output trajectories and plots
+    # data_static = pandas.read_csv(
+    #     os.path.join(TDIR, "..", "Input", "GrCM_static.txt"), sep="\t")
+    # data_inp = pandas.read_csv(
+    #     os.path.join(TDIR, "..", "Input", "GrCM_input.txt"), sep="\t")
+    # GrCM(data_static, data_inp)
+
+    # Get input from chanels (supplied by file or another model)
+    in1 = CisPandasInput('GrCM_input1')
+    in2 = CisPandasInput('GrCM_static')
+    out1 = CisPandasOutput('GrCM_output')
+
+    flag, data_static = in2.recv()
+    if not flag:
+        raise Exception("GrCM: Error receiving from GrCM_static")
+
+    while True:
+        flag, data_inp = in1.recv()
+        if not flag:
+            print("GrCM: No more input from GrCM_input1")
+            break
+        data_static = GrCM(data_static, data_inp, save_trajectories=False,
+                           plot_trajectories=False)
+        out1.send(data_static)
+
+        
 # The ``driver`` that will integrate the ODE(s):
 if __name__ == '__main__':
     main()
